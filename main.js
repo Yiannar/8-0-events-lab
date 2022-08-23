69d3bb9a18d65d789e1c21dce359988c3c854a3d
Yiannar <yiannarichey@pursuit.org>
2022-08-23 14:41:15 -0400

    Finished lab with Mina, Edgy, and Melanie

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 // Do not change the code below.
 const main = document.querySelector("main");
 for (let i = 0; i < 100; i++) {
-  const div = document.createElement("div");
-  div.classList.add("cell");
-  main.append(div);
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    main.append(div);
 }
 
 // You may write your code here!
+
+// const palette = document.querySelector("#palette")
+// palette.addEventListener("click", () => {
+//  palette.style
+// })
+
+const divColors = document.querySelectorAll(".color")
+console.log(divColors)
+
+const currentColor = document.querySelector("#current-color")
+
+divColors.forEach((color) => {
+    color.addEventListener("click", () => {
+        currentColor.style.backgroundColor = color.style.backgroundColor
+    })
+})
+
+const cells = document.querySelectorAll("#canvas .cell")
+
+cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+        cell.style.backgroundColor = currentColor.style.backgroundColor
+    })
+})
